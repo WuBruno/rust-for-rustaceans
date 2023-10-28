ffb92c16d008ab82591cfa51b450b3808a64607e
Bruno Wu <wubruno@outlook.com>
2023-10-28 18:44:23 +0100

    Page 14

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused_variables)]
+
+mod rust_lang;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -26,10 +30,51 @@ mod tests {
 
             let was = std::mem::take(s);
             assert_eq!(*was, 42);
-            assert_eq!(*s, Box::new(0)); // changed to default value
+            assert_eq!(*s, Box::new(0)); // changed to default value of 0
+
+            *s = was;
+            assert_eq!(**s, 42);
+
+            let mut r = Box::new(84);
+            std::mem::swap(s, &mut r);
+            assert_eq!(*r, 42);
+            assert_eq!(**s, 84);
         }
 
         let mut s = Box::new(42);
         replace_with_84(&mut s);
+    }
+
+    #[test]
+    fn listing_1_8() {
+        fn rand() -> f32 {
+            0.1
+        }
+
+        let mut x = Box::new(42);
+        let r = &x;
+        if rand() > 0.5 {
+            *x = 84;
+            // dbg!(r);
+        } else {
+            dbg!(r);
+        }
+
+        // dbg!(r);
+        dbg!(x);
+    }
+
+    #[test]
+    fn listing_1_9() {
+        let mut x = Box::new(42);
+        let mut z = &x;
+        for i in 0..100 {
+            dbg!(z);
+            x = Box::new(i);
+            // dbg!(z); illegal – lifetime dead here
+            z = &x; // resumed here. Without this it would not work
+        }
+
+        dbg!(z);
     }
 }
---
+++ src/rust_lang.rs
@@ -0,0 +1,21 @@
+/// Rust Programming Language Book
+
+#[cfg(test)]
+mod tests2 {
+    #[test]
+    fn listing_10_19() {
+        fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+            if x.len() > y.len() {
+                x
+            } else {
+                y
+            }
+        }
+
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
+
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
+}
